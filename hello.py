a2d4c17316c00ba788760be3c5635924cd2f7160
bhimAcquia <bhim.sella@acquia.com>
2021-06-30 15:36:28 +0530

    created hello.py
    python code for git pull

---
+++ hello.py
@@ -0,0 +1,24 @@
+import git
+from git import Repo
+
+# rorepo is a Repo instance pointing to the git-python repository.
+# For all you know, the first argument to Repo is a path to the repository
+# you want to work with
+print("hello python")
+
+def git_call() :
+    print("git_call")
+    #git_dir = "https://github.com/bhimAcquia/airflow-sbt-poc.git"
+    #repo = Repo(git_dir)
+    #repo.config_reader()             # get a config reader for read-only access
+    #repo.remotes.pull()
+    git_dir = ""
+    g = git.Git( git_dir )
+    print("git reset --hard")
+    g.reset('--hard')
+    print("git pull")
+    g.pull()
+    #git.cmd.Git().pull(git_dir,'master')
+    print("git pull done")
+
+git_call()
